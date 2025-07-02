eee02241569069073d62292de9850e850766c65a
DESKTOP-2URSNIJ\202_19 <hjpp3983@gmail.com>
2025-07-02 16:27:02 +0900

    7번째 커밋

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.linearVelocity = transform.forward * speed;
+        Destroy(gameObject, 3f); // Destroy the bullet after 2 seconds
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController playerController
+                = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Die(); // Assuming TakeDamage is a method in PlayerController
+            }
+        }
+        // Update is called once per frame
+    }
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,46 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+
+
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
+        //수평과 수직 축 입력 값을 감지하여 저장
+        float xinput = Input.GetAxis("Horizontal");
+        Debug.Log("xinput: " + xinput);
+        float zinput = Input.GetAxis("Vertical");
+        Debug.Log("zinput: " + zinput);
+
+        //실제 이동 속도를 입력 값과 이동 속력을 통해 결정
+        float xSpeed = xinput * speed;
+        float zSpeed = zinput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+        playerRigidbody.linearVelocity = newVelocity;
+
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+        GameManager gameManager = FindAnyObjectByType<GameManager>();
+
+        gameManager.EndGame();
+
     }
 }
+
